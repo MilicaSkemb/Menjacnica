f9c524c99453cb848ab6be7686ece2f02ac2cdf2
Milica Skembarevic <milicaskembarevic@gmail.com>
2017-04-05 22:03:25 +0200

    Uvedena klasa MenjanjePara koja implementira interfejs specifikacija.

---
+++ Menjacnica/src/menjacnica/MenjanjePara.java
@@ -0,0 +1,28 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import specifikacija.Specifikacija;
+
+public class MenjanjePara implements Specifikacija {
+
+	@Override
+	public void dodavanjeKursa(GregorianCalendar datumKursa, double prodajniKurs, double srednjiKurs,
+			double kupovniKurs) {
+		
+
+	}
+
+	@Override
+	public void obrisiKurs(GregorianCalendar datumKursa) {
+		
+
+	}
+
+	@Override
+	public Kursevi nadjiKursZaOdredjeniDatum(GregorianCalendar datumKursa) {
+		
+		return null;
+	}
+
+}
