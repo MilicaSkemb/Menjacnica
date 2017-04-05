7dda2010d391d15455cf2808c68b6f48581a47ca
Milica Skembarevic <milicaskembarevic@gmail.com>
2017-04-05 22:10:35 +0200

    Implementirana metoda za dodavanje kursa u klasi MenjanjePara.

--- Menjacnica/src/menjacnica/MenjanjePara.java
+++ Menjacnica/src/menjacnica/MenjanjePara.java
@@ -5,14 +5,17 @@ import java.util.GregorianCalendar;
 import specifikacija.Specifikacija;
 
 public class MenjanjePara implements Specifikacija {
-
+	private Valuta v = new Valuta();
 	@Override
 	public void dodavanjeKursa(GregorianCalendar datumKursa, double prodajniKurs, double srednjiKurs,
 			double kupovniKurs) {
-		
-
+		Kursevi k = new Kursevi();
+		k.setProdajniKurs(prodajniKurs);
+		k.setSrednjiKurs(srednjiKurs);
+		k.setDatumKursa(datumKursa);
+		k.setKupovniKurs(kupovniKurs);
+		v.getKurseviValute().add(k);
 	}
-
 	@Override
 	public void obrisiKurs(GregorianCalendar datumKursa) {
 		
