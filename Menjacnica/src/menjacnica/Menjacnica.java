0609bfcdf4b860e3ca78e6dc26a4a70be74c8394
Milica Skembarevic <milicaskembarevic@gmail.com>
2017-04-05 21:47:49 +0200

    Napravljene metode: toString, equals i hashCode za sve klase.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -10,4 +10,33 @@ public class Menjacnica {
 	public void setNaziv(Valuta naziv) {
 		this.naziv = naziv;
 	}
+	@Override
+	public String toString() {
+		return "Menjacnica " + naziv;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Menjacnica other = (Menjacnica) obj;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		return true;
+	}
 }
--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -24,4 +24,44 @@ public class Valuta {
 	public void setKurseviValute(LinkedList<Kursevi> kurseviValute) {
 		this.kurseviValute = kurseviValute;
 	}
+	@Override
+	public String toString() {
+		return "Valuta Naziv = " + naziv + ", Skraceni naziv valute = " + skraceniNazivValute + ", Kursevi valute ="
+				+ kurseviValute;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kurseviValute == null) ? 0 : kurseviValute.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skraceniNazivValute == null) ? 0 : skraceniNazivValute.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (kurseviValute == null) {
+			if (other.kurseviValute != null)
+				return false;
+		} else if (!kurseviValute.equals(other.kurseviValute))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skraceniNazivValute == null) {
+			if (other.skraceniNazivValute != null)
+				return false;
+		} else if (!skraceniNazivValute.equals(other.skraceniNazivValute))
+			return false;
+		return true;
+	}
 }
